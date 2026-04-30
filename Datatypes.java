1a8ab7ec50bb307795795306fe6bc3a55c95fdfe
Mohammed_Shahid <mohammedshahidcoding@gmail.com>
2026-04-29 22:29:30 -0700

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+class Datatypes
+{
+public static void main(String[]args)
+{
+byte b1,b2;
+b1=120;
+b2=-48;
+System.out.println("The byte value is:"+b1);
+System.out.println("The byte value is:"+b2);
+
+short s1,s2;
+s1=7000;
+s2=-500;
+System.out.println("The byte short is:"+s1);
+System.out.println("The byte short is:"+s2);
+
+int i1,i2;
+i1=9000;
+i2=-299;
+System.out.println("The byte integer is:"+i1);
+System.out.println("The byte integer is:"+i2);
+
+}
+}
